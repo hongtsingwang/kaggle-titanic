1c3ca9343f55576b690ee027356a25d1769599d1
wanghongqing <wanghongqing@le.com>
2017-05-11 19:23:26 +0800

    merge together

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 # =======================================================
-# 
+#
 # @FileName  : main.py
 # @Author    : Wang Hongqing
 # @Date      : 2017-05-11 14:07
-# 
+#
 # =======================================================
 
 import os
@@ -15,8 +15,10 @@ import numpy as np
 import sklearn.preprocessing as preprocessing
 from pandas import DataFrame
 from sklearn import linear_model
-from script.age_engineering import set_missing_ages
-from script.age_engineering import set_Cabin_type
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import BaggingRegressor
+
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -47,31 +49,46 @@ make_dir(result_dir)
 make_dir(script_dir)
 
 
+def dummies_feature(data_frame):
+    data_frame = set_Cabin_type(data_frame)
+    dummies_Cabin = pd.get_dummies(data_frame['Cabin'], prefix='Cabin')
+    dummies_Embarked = pd.get_dummies(
+        data_frame['Embarked'], prefix='Embarked')
+    dummies_Sex = pd.get_dummies(data_frame['Sex'], prefix='Sex')
+    dummies_Pclass = pd.get_dummies(data_frame['Pclass'], prefix='Pclass')
+    df = pd.concat([data_frame, dummies_Cabin, dummies_Embarked,
+                    dummies_Sex, dummies_Pclass], axis=1)
+    df.drop(['Pclass', 'Name', 'Sex', 'Ticket',
+             'Cabin', 'Embarked'], axis=1, inplace=True)
+    return df
+
+
+# path define
+home_dir = os.getcwd()
+data_dir = os.path.join(home_dir, "data")
+result_dir = os.path.join(home_dir, "result")
+train_file = os.path.join(data_dir, "train.csv")
+test_file = os.path.join(data_dir, "test.csv")
+predict_file = os.path.join(result_dir, "submission.csv")
+
+make_dir(data_dir)
+make_dir(result_dir)
+
+
 data_train = pd.read_csv(train_file)
 data_train, rfr = set_missing_ages(data_train)
-data_train = set_Cabin_type(data_train)
 
-dummies_Cabin = pd.get_dummies(data_train['Cabin'], prefix='Cabin')
-dummies_Embarked = pd.get_dummies(data_train['Embarked'], prefix='Embarked')
-dummies_Sex = pd.get_dummies(data_train['Sex'], prefix='Sex')
-dummies_Pclass = pd.get_dummies(data_train['Pclass'], prefix='Pclass')
-
-df = pd.concat([data_train, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
-df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+df = dummies_feature(data_train)
 scaler = preprocessing.StandardScaler()
 age_scale_param = scaler.fit(df['Age'])
 df['Age_scaled'] = scaler.fit_transform(df['Age'], age_scale_param)
 fare_scale_param = scaler.fit(df['Fare'])
 df['Fare_scaled'] = scaler.fit_transform(df['Fare'], fare_scale_param)
 
-# scaler = preprocessing.StandardScaler()
-# age_scale_param = scaler.fit(df['Age'])
-# df['Age_scaled'] = scaler.fit_transform(df['Age'], age_scale_param)
-# fare_scale_param = scaler.fit(df['Fare'])
-# df['Fare_scaled'] = scaler.fit_transform(df['Fare'], fare_scale_param)
 
 # 用正则取出我们要的属性值
-train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+train_df = df.filter(
+    regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
 train_np = train_df.as_matrix()
 
 y = train_np[:, 0]
@@ -81,7 +98,7 @@ clf.fit(X, y)
 
 # test_data处理
 data_test = pd.read_csv(test_file)
-data_test.loc[(data_test.Fare.isnull()), 'Fare'] = 0
+data_test.loc[(data_test['Fare'].isnull()), 'Fare'] = 0
 tmp_df = data_test[['Age', 'Fare', 'Parch', 'SibSp', 'Pclass']]
 null_age = tmp_df[data_test.Age.isnull()].as_matrix()
 
@@ -89,102 +106,39 @@ null_age = tmp_df[data_test.Age.isnull()].as_matrix()
 X = null_age[:, 1:]
 predictedAges = rfr.predict(X)
 data_test.loc[(data_test.Age.isnull()), 'Age'] = predictedAges
-data_test = set_Cabin_type(data_test)
-dummies_Cabin = pd.get_dummies(data_test['Cabin'], prefix='Cabin')
-dummies_Embarked = pd.get_dummies(data_test['Embarked'], prefix='Embarked')
-dummies_Sex = pd.get_dummies(data_test['Sex'], prefix='Sex')
-dummies_Pclass = pd.get_dummies(data_test['Pclass'], prefix='Pclass')
-
-df_test = pd.concat([data_test, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
-df_test.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+df_test = dummies_feature(data_test)
 df_test['Age_scaled'] = scaler.fit_transform(df_test['Age'], age_scale_param)
-df_test['Fare_scaled'] = scaler.fit_transform(df_test['Fare'], fare_scale_param)
+df_test['Fare_scaled'] = scaler.fit_transform(
+    df_test['Fare'], fare_scale_param)
 
-test = df_test.filter(regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+test = df_test.filter(
+    regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
 predictions = clf.predict(test)
-result = DataFrame({'PassengerId': data_test['PassengerId'].as_matrix(), 'Survived': predictions.astype(np.int32)})
-# result.to_csv(predict_file, index=False)
 
 # -----------------------------------------交叉验证------------------------------------------------------------------
 from sklearn import cross_validation
 
 clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
-all_data = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+all_data = df.filter(
+    regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
 X = all_data.as_matrix()[:, 1:]
 y = all_data.as_matrix()[:, 0]
 print cross_validation.cross_val_score(clf, X, y, cv=5)
 
-split_train, split_cv = cross_validation.train_test_split(df, test_size=0.3, random_state=0)
-train_df = split_train.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+split_train, split_cv = cross_validation.train_test_split(
+    df, test_size=0.3, random_state=0)
+train_df = split_train.filter(
+    regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
 clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
 clf.fit(train_df.as_matrix()[:, 1:], train_df.as_matrix()[:, 0])
 # 对cross validation数据进行预测
-cv_df = split_cv.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+cv_df = split_cv.filter(
+    regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
 predictions = clf.predict(cv_df.as_matrix()[:, 1:])
 origin_data_train = pd.read_csv(train_file)
 bad_cases = origin_data_train.loc[
     origin_data_train['PassengerId'].isin(split_cv[predictions != cv_df.as_matrix()[:, 0]]['PassengerId'].values)]
 
-# ------------------------------写learning curve ----------------------------------------------------------------
-# import matplotlib.pyplot as plt
-from sklearn.learning_curve import learning_curve
-
-
-def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=1
-                        , train_sizes=np.linspace(0.05, 1.0, 20), verbose=0, plot=True):
-    """
-    画data在模型上面的learning curve
-    :param estimator: 分类器名称
-    :param title: 表格的标题
-    :param X: numpy,类型， 输入的feature
-    :param y:
-    :param ylim: （ymin, ymax）:  纵坐标的最低点和最高点 
-    :param cv: 做cross-validation的时候， 份数。 一般一份是cv集， 另外n-1是训练集
-    :param n_jobs:  并行的任务数
-    :param train_sizes: 
-    :param verbose: 
-    :param plot: 
-    :return: 
-    """
-    train_sizes, train_scores, test_scores = learning_curve(estimator, X, y, train_sizes, cv, n_jobs=n_jobs,
-                                                            verbose=verbose)
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.mean(test_scores, axis=1)
-
-    if plot:
-        plt.figure()
-        plt.title(title)
-        if ylim:
-            plt.ylim(*ylim)  # TODO ???
-        plt.xlabel(u"训练样本数")
-        plt.ylabel(u"得分")
-        plt.gca().invert_yaxis()  # TODO ???
-        plt.grid()
-
-        plt.fill_between(train_sizes,
-                         train_scores_mean - train_scores_std,
-                         train_scores_mean + train_scores_std, alpha=0.1, color="b"
-                         )
-        plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1,
-                         color="r")
-        plt.plot(train_sizes, train_scores_mean, 'o-', color="b", label=u"训练集上得分")
-        plt.plot(train_sizes, test_scores_mean, 'o-', color="r", label=u"交叉验证集上得分")
-        plt.legend(loc="best")
-        plt.draw()
-        plt.show()
-        plt.gca().invert_yaxis()
-
-    mid_point = ((train_scores_mean[-1] + train_scores_std[-1]) + (test_scores_mean[-1] - test_scores_std[-1])) / 2
-    diff = (train_scores_mean[-1] + train_scores_std[-1]) - (test_scores_mean[-1] - test_scores_std[-1])
-    return mid_point, diff
-
-
-# plot_learning_curve(clf, u"学习曲线", X, y)
-
-# ------------------模型融合--------------------------------------------------------
-from sklearn.ensemble import BaggingRegressor
 
 train_df = df.filter(
     regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass.*|Mother|Child|Family|Title')
@@ -197,7 +151,9 @@ bagging_clf = BaggingRegressor(clf, n_estimators=20, max_samples=0.8, max_featur
                                bootstrap_features=False, n_jobs=-1)
 bagging_clf.fit(X, y)
 
-test = df_test.filter(regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass.*|Mother|Child|Family|Title')
+test = df_test.filter(
+    regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass.*|Mother|Child|Family|Title')
 predictions = bagging_clf.predict(test)
-result = pd.DataFrame({'PassengerId': data_test['PassengerId'].as_matrix(), 'Survived': predictions.astype(np.int32)})
+result = pd.DataFrame({'PassengerId': data_test['PassengerId'].as_matrix(
+), 'Survived': predictions.astype(np.int32)})
 result.to_csv(predict_file, index=False)
